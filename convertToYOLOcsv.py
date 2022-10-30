c6f8675031212c5c522b5226c9871ae3819e9c87
edinitu <80175654+edinitu@users.noreply.github.com>
2022-10-30 13:56:38 +0200

    Add files via upload

---
+++ convertToYOLOcsv.py
@@ -0,0 +1,81 @@
+import torch
+import csv
+import os
+
+class Annotation:
+    def __init__(self, clazz, x_center, y_center, width, height):
+        self.__clazz = clazz
+        self.__x_center = x_center
+        self.__y_center = y_center
+        self.__width = width
+        self.__height = height
+
+    @property
+    def clazz(self):
+        return self.__clazz
+
+    @property
+    def x_center(self):
+        return self.__x_center
+
+    @property
+    def y_center(self):
+        return self.__y_center
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+
+TRAIN_LABELS_PATH = r'C:\Users\edini\Desktop\licenta\data_set\training_set\labelTxt-v1.5\DOTA-v1.5_train_hbb'
+TRAIN_LABELS_PATH_CSV = r'C:\Users\edini\Desktop\licenta\data_set\training_set\labelTxt-v1.5\csv_annotations'
+
+annotations_list = []
+
+
+def convertToYOLO(bbox):
+    yolo_labels = []
+    x_midpoint = (float(bbox[0])+float(bbox[6]))/2
+    y_midpoint = (float(bbox[1])+float(bbox[7]))/2
+
+    height = abs(float(bbox[1])-float(bbox[7]))
+    width = abs(float(bbox[0])-float(bbox[2]))
+
+    yolo_labels.append(x_midpoint)
+    yolo_labels.append(y_midpoint)
+    yolo_labels.append(width)
+    yolo_labels.append(height)
+
+    return yolo_labels
+
+
+for filename in os.listdir(TRAIN_LABELS_PATH):
+    f = os.path.join(TRAIN_LABELS_PATH, filename)
+    if os.path.isfile(f):
+        with open(f, 'r') as file:
+            while True:
+                file.readline()
+                file.readline()
+                line = file.readline().split(' ')
+                if line == ['']:
+                    break
+                bbox = line[0:8]
+                yolo_format = [int(line[9].replace('\n', ''))]
+                yolo_format.extend(convertToYOLO(bbox))
+                
+                annotation = Annotation(yolo_format[0], yolo_format[1],
+                                        yolo_format[2], yolo_format[3], yolo_format[4])
+                annotations_list.append([annotation.clazz, annotation.x_center, annotation.y_center, annotation.width, annotation.height])
+
+        csvfilename = filename.replace('.txt', '.csv')
+        with open(os.path.join(TRAIN_LABELS_PATH_CSV, csvfilename),
+                  'w', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerows(annotations_list)
+            annotations_list.clear()
+
+
---
+++ showImageFromDataset.py
@@ -0,0 +1,86 @@
+import os
+import matplotlib.pyplot as plt
+
+
+class ImageElement:
+    def __init__(self):
+        self.bounding_box = []
+        self.label = ''
+        self.number_label = 0
+        self.image_metadata = ImageMetadata()
+
+    def set_bounding_box(self, coordinates):
+        for coordinate in coordinates:
+            self.bounding_box.append(float(coordinate))
+
+    def set_label(self, label):
+        self.label = label
+
+    def set_number_label(self, number_label):
+        self.number_label = number_label
+
+    def set_image_metadata(self, image_metadata):
+        self.image_metadata = image_metadata
+
+    def get_bounding_box(self):
+        return self.bounding_box
+
+    def draw_box(self):
+        x = []
+        y = []
+        for i in range(len(self.bounding_box)):
+            if i % 2 == 0:
+                x.append(self.bounding_box[i])
+            else:
+                y.append(self.bounding_box[i])
+        plt.plot(x[0:2], y[0:2], color='black', label=self.label)
+        plt.plot(x[1:3], y[1:3], color='black')
+        plt.plot(x[2:4], y[2:4], color='black')
+        plt.plot([x[3], x[0]], [y[3], y[0]], color='black')
+
+
+class ImageMetadata:
+    def __init__(self):
+        self.image_source = ''
+        self.gsd = ''
+
+    def set_image_source(self, img_source):
+        self.image_source = img_source
+
+    def set_gsd(self, gsd):
+        self.gsd = gsd
+
+
+def read_one_image_labels(img_label):
+    with open(img_label, 'r') as file:
+        img_elements = []
+        img_data = ImageMetadata()
+        img_data.set_image_source(file.readline().split(':')[1].strip('\n'))
+        img_data.set_gsd(file.readline().split(':')[1].strip('\n'))
+        while True:
+            img_element = ImageElement()
+            img_element.set_image_metadata(img_data)
+            line = file.readline().split(' ')
+            if line == ['']:
+                break
+            img_element.set_bounding_box(line[0:8])
+            img_element.set_label(line[8])
+            img_element.set_number_label(line[9].strip('\n'))
+            img_elements.append(img_element)
+    return img_elements
+
+
+TRAIN_DATA_PATH = r'C:\Users\edini\Desktop\licenta\data_set\training_set\images'
+TRAIN_LABELS_PATH = r'C:\Users\edini\Desktop\licenta\data_set\training_set\labelTxt-v1.5\DOTA-v1.5_train_hbb'
+
+item = input()
+elements = read_one_image_labels(os.path.join(TRAIN_LABELS_PATH, item + '.txt'))
+img = plt.imread(os.path.join(TRAIN_DATA_PATH, item +'.png'))
+
+plt.figure()
+for element in elements:
+    element.draw_box()
+
+plt.imshow(img)
+plt.plot()
+plt.show()
