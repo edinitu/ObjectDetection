Fix image normalization
Signed-off-by: edinitu <eduard.nitu@stud.etti.upb.ro>

customDataset.py
@@ -42,10 +42,11 @@ class AerialImagesDataset(Dataset):
         if image.shape == (448, 448, 4):
             image = image[:, :, 0:3]
 
-      #
-        image = (image - np.mean(image)) / np.std(image)
-        image = (image - np.min(image)) / (np.max(image) - np.mean(image))
 
+      #  image = (image - np.mean(image)) / np.std(image)
+        image = (image - np.min(image)) / (np.max(image) - np.min(image))
+        if np.min(image) < 0 or np.max(image) > 1:
+            raise RuntimeError(f'Image values out of range: max {np.max(image)}, min {np.min(image)}')
         annotations = np.array(self.annotations[idx])
         # get the vectors for every grid cell in the image
         grids_annotations = self.build_grids_annotations(annotations)

model.py
@@ -26,7 +26,7 @@ class NetworkModel(torch.nn.Module):
         self.conv16 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, padding=1)
         self.conv17 = nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=1)
         self.conv18 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, padding=1)
-      #  self.batch_norm1 = nn.BatchNorm2d(1024)
+        self.batch_norm1 = nn.BatchNorm2d(1024)
         self.conv19 = nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=1)
         self.conv20 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, padding=1)
         self.conv21 = nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3, padding=1)
@@ -76,7 +76,7 @@ class NetworkModel(torch.nn.Module):
         x = self.fc1(x)
         x = nn.functional.leaky_relu(x, negative_slope=0.1)
         x = nn.functional.dropout(x, p=0.5)
-        x = nn.functional.sigmoid(self.fc2(x))
+        x = nn.functional.relu(self.fc2(x))
 
         #print('Final shape of tensor:', str(x.shape))
         return x

training.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
     network = model.NetworkModel()
 
     print('Loading the dataloader...')
-    dataloader = DataLoader(dataset=aerial_dataset, batch_size=4, shuffle=True, num_workers=1)
+    dataloader = DataLoader(dataset=aerial_dataset, batch_size=64, shuffle=True, num_workers=1)
     print('Dataloader ready')
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')